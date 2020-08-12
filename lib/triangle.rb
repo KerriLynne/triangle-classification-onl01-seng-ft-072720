43d78878251bbfbab4a2fbe99e59f5b2dcefc9a4
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-12 21:08:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,4 +3,15 @@ class Triangle
     @x = x
     @y = y
     @z = z
+  end
+
+  def kind
+    if (@x <= 0) || (@y <= 0) || (@z <= 0)
+      raise TriangleError
+    elsif (@x+@y <= @z) || (@x+@z <= @y) || (@y+@z <= @x)
+      raise TriangleError
+    else
+      if (@x == @y) && (@y == @z)
+        :equilateral
+      elsif (@x == @y) || (@y == @z) || (@x == @z)  
 end
